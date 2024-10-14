abfb112f81b5f97913ea694dc85393877aea98ae
Vu <vudang30112002@gmail.com>
2024-10-14 20:44:45 +0700

    .

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import { ESLint } from 'eslint';
+
+export default [
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      prettier: require('eslint-plugin-prettier'),
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'prettier/prettier': 'error',
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended',
+    ],
+  },
+];
