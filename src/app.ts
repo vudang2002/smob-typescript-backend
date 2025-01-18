b8682a1af3f1c6bcc96e0ea59e1b3d0aaee930f3
thyphamdev <landang@Lans-MacBook-Pro.local>
2025-01-18 12:26:26 +0100

    [SMOB-17] code review

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,33 @@
 import express from 'express';
-import pkg, { PoolClient } from 'pg';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 dotenv.config();
 
-const { Pool } = pkg;
-
+const prisma = new PrismaClient();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Create a new pool instance
-const pool = new Pool({
-  user: process.env.PGUSER || 'postgres',
-  host: process.env.PGHOST || 'localhost',
-  database: process.env.PGDATABASE || 'wecommit',
-  password: process.env.PGPASSWORD || '30112002',
-  port: parseInt(process.env.PGPORT || '5433', 10),
-});
+async function start() {
+  try {
+    await prisma.$connect();
+    console.log('Database is connected!');
+  } catch (error) {
+    console.error(
+      "Couldn't conenct to the database: ",
+      (error as Error).message,
+    );
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 
-// Test the database connection
-pool.connect(
-  (err: Error | undefined, client: PoolClient | undefined, release) => {
-    if (err) {
-      return console.error('Error acquiring client', err.stack);
-    }
-    if (client) {
-      client.query('SELECT NOW()', (err, result) => {
-        release();
-        if (err) {
-          return console.error('Error executing query', err.stack);
-        }
-        console.log('Connected to the database:', result.rows);
-      });
-    }
-  },
-);
+  app.get('/health', async (req, res) => {
+    res.send('OK');
+  });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
 
-export default app;
+start();
